45344f9561a8a9b53c88fe61539e4246c3839f36
Clonescody <clonescody@protonmail.com>
2023-01-16 07:28:53 -0800

    update contracts and fix wa issues

--- src/aggregate-vault.ts
+++ src/aggregate-vault.ts
@@ -1,4 +1,4 @@
-import { ethereum } from "@graphprotocol/graph-ts";
+import { Address, ethereum } from "@graphprotocol/graph-ts";
 import {
   AggregateVault,
   CloseRebalance,
@@ -13,7 +13,15 @@ import {
   VaultPricePerShare,
   VaultTVL,
 } from "../generated/schema";
-import { AGGREGATE_VAULT_ADDRESS, VAULTS_ARRAY } from "./constants";
+import {
+  AGGREGATE_VAULT_ADDRESS,
+  LINK_VAULT_ADDRESS,
+  UNI_VAULT_ADDRESS,
+  USDC_VAULT_ADDRESS,
+  VAULTS_ARRAY,
+  WBTC_VAULT_ADDRESS,
+  WETH_VAULT_ADDRESS,
+} from "./constants";
 
 // It is also possible to access smart contracts from mappings. For
 // example, the contract that has emitted the event can be connected to
@@ -53,32 +61,119 @@ import { AGGREGATE_VAULT_ADDRESS, VAULTS_ARRAY } from "./constants";
 
 export function handleBlock(block: ethereum.Block): void {
   const aggregateVaultContract = AggregateVault.bind(AGGREGATE_VAULT_ADDRESS);
-  VAULTS_ARRAY.map((vaultAddress) => {
-    const entityId = `${block.timestamp}:${vaultAddress}`;
 
-    /** Vault price per share */
-    const vaultPps = new VaultPricePerShare(entityId);
-    vaultPps.block = block.number;
-    vaultPps.timestamp = block.timestamp;
-    vaultPps.vault = vaultAddress.toString();
-    vaultPps.pricePerShare = aggregateVaultContract.getVaultPPS(vaultAddress);
-    vaultPps.save();
-  });
+  /** USDC vault price per share */
+  const usdcVaultEntityId = `${block.timestamp}:${USDC_VAULT_ADDRESS}`;
+  const usdcVaultPps = new VaultPricePerShare(usdcVaultEntityId);
+
+  usdcVaultPps.block = block.number;
+  usdcVaultPps.timestamp = block.timestamp;
+  usdcVaultPps.vault = USDC_VAULT_ADDRESS.toString();
+  usdcVaultPps.pricePerShare = aggregateVaultContract.getVaultPPS(
+    USDC_VAULT_ADDRESS
+  );
+  usdcVaultPps.save();
+
+  /** WETH vault price per share */
+  const wethVaultEntityId = `${block.timestamp}:${WETH_VAULT_ADDRESS}`;
+  const wethVaultPps = new VaultPricePerShare(wethVaultEntityId);
+
+  wethVaultPps.block = block.number;
+  wethVaultPps.timestamp = block.timestamp;
+  wethVaultPps.vault = WETH_VAULT_ADDRESS.toString();
+  wethVaultPps.pricePerShare = aggregateVaultContract.getVaultPPS(
+    WETH_VAULT_ADDRESS
+  );
+  wethVaultPps.save();
+
+  /** WBTC vault price per share */
+  const wbtcVaultEntityId = `${block.timestamp}:${WBTC_VAULT_ADDRESS}`;
+  const wbtcVaultPps = new VaultPricePerShare(wbtcVaultEntityId);
+
+  wbtcVaultPps.block = block.number;
+  wbtcVaultPps.timestamp = block.timestamp;
+  wbtcVaultPps.vault = WBTC_VAULT_ADDRESS.toString();
+  wbtcVaultPps.pricePerShare = aggregateVaultContract.getVaultPPS(
+    WBTC_VAULT_ADDRESS
+  );
+  wbtcVaultPps.save();
+
+  /** UNI vault price per share */
+  const uniVaultEntityId = `${block.timestamp}:${UNI_VAULT_ADDRESS}`;
+  const uniVaultPps = new VaultPricePerShare(uniVaultEntityId);
+
+  uniVaultPps.block = block.number;
+  uniVaultPps.timestamp = block.timestamp;
+  uniVaultPps.vault = UNI_VAULT_ADDRESS.toString();
+  uniVaultPps.pricePerShare = aggregateVaultContract.getVaultPPS(
+    UNI_VAULT_ADDRESS
+  );
+  uniVaultPps.save();
+
+  /** LINK vault price per share */
+  const linkVaultEntityId = `${block.timestamp}:${LINK_VAULT_ADDRESS}`;
+  const linkVaultPps = new VaultPricePerShare(linkVaultEntityId);
+
+  linkVaultPps.block = block.number;
+  linkVaultPps.timestamp = block.timestamp;
+  linkVaultPps.vault = LINK_VAULT_ADDRESS.toString();
+  linkVaultPps.pricePerShare = aggregateVaultContract.getVaultPPS(
+    LINK_VAULT_ADDRESS
+  );
+  linkVaultPps.save();
 }
 
 export function handleCloseRebalance(event: CloseRebalance): void {
   const aggregateVaultContract = AggregateVault.bind(AGGREGATE_VAULT_ADDRESS);
-  VAULTS_ARRAY.map((vaultAddress) => {
-    const entityId = `${event.block.timestamp}:${vaultAddress}`;
+  /** USDC vault price per share */
+  const usdcVaultEntityId = `${event.block.timestamp}:${USDC_VAULT_ADDRESS}`;
+  const usdcVaultTvl = new VaultTVL(usdcVaultEntityId);
 
-    /** Vault TVL */
-    const vaultTvl = new VaultTVL(entityId);
-    vaultTvl.block = event.block.number;
-    vaultTvl.timestamp = event.block.timestamp;
-    vaultTvl.vault = vaultAddress.toString();
-    vaultTvl.tvl = aggregateVaultContract.getVaultTVL(vaultAddress);
-    vaultTvl.save();
-  });
+  usdcVaultTvl.block = event.block.number;
+  usdcVaultTvl.timestamp = event.block.timestamp;
+  usdcVaultTvl.vault = USDC_VAULT_ADDRESS.toString();
+  usdcVaultTvl.tvl = aggregateVaultContract.getVaultTVL(USDC_VAULT_ADDRESS);
+  usdcVaultTvl.save();
+
+  /** WETH vault TVL */
+  const wethVaultEntityId = `${event.block.timestamp}:${WETH_VAULT_ADDRESS}`;
+  const wethVaulTvl = new VaultTVL(wethVaultEntityId);
+
+  wethVaulTvl.block = event.block.number;
+  wethVaulTvl.timestamp = event.block.timestamp;
+  wethVaulTvl.vault = WETH_VAULT_ADDRESS.toString();
+  wethVaulTvl.tvl = aggregateVaultContract.getVaultTVL(WETH_VAULT_ADDRESS);
+  wethVaulTvl.save();
+
+  /** WBTC vault TVL */
+  const wbtcVaultEntityId = `${event.block.timestamp}:${WBTC_VAULT_ADDRESS}`;
+  const wbtcVaultTvl = new VaultTVL(wbtcVaultEntityId);
+
+  wbtcVaultTvl.block = event.block.number;
+  wbtcVaultTvl.timestamp = event.block.timestamp;
+  wbtcVaultTvl.vault = WBTC_VAULT_ADDRESS.toString();
+  wbtcVaultTvl.tvl = aggregateVaultContract.getVaultTVL(WBTC_VAULT_ADDRESS);
+  wbtcVaultTvl.save();
+
+  /** UNI vault TVL */
+  const uniVaultEntityId = `${event.block.timestamp}:${UNI_VAULT_ADDRESS}`;
+  const uniVaultTvl = new VaultTVL(uniVaultEntityId);
+
+  uniVaultTvl.block = event.block.number;
+  uniVaultTvl.timestamp = event.block.timestamp;
+  uniVaultTvl.vault = UNI_VAULT_ADDRESS.toString();
+  uniVaultTvl.tvl = aggregateVaultContract.getVaultTVL(UNI_VAULT_ADDRESS);
+  uniVaultTvl.save();
+
+  /** LINK vault TVL */
+  const linkVaultEntityId = `${event.block.timestamp}:${LINK_VAULT_ADDRESS}`;
+  const linkVaultTvl = new VaultTVL(linkVaultEntityId);
+
+  linkVaultTvl.block = event.block.number;
+  linkVaultTvl.timestamp = event.block.timestamp;
+  linkVaultTvl.vault = LINK_VAULT_ADDRESS.toString();
+  linkVaultTvl.tvl = aggregateVaultContract.getVaultTVL(LINK_VAULT_ADDRESS);
+  linkVaultTvl.save();
 }
 
 export function handleCollectVaultFees(event: CollectVaultFees): void {
